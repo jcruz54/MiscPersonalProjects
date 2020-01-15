61f0feb94959cf9cea6e12798f23bcbe73b42465
JReyes54 <stacruzjulian54@gmail.com>
2020-01-15 09:14:15 -0800

    Made blank static methods brute_force_decrypt() and decrypt()

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -1,27 +1,43 @@
 
-def caesar_encrypt(plaintext_message, shift):
-    """
-        Encrypts a string using a Caesar Cipher with a user-provided shift.
-        Returns the encrypted message.
-    """
-    plaintext_message = plaintext_message.lower()
+class CaesarCipher():
+
     alphabet = "abcdefghijklmnopqrstuvwxyz"
-    encrypted_message = ""
 
-    for char in plaintext_message:
-        
-        # Skip encryption if char is non-alphabetical
-        if char not in alphabet:
-            encrypted_message += char
-            continue
+    @staticmethod
+    def brute_force_decrypt(encrypted_message):
+        """
+        """
+        pass
 
-        char_alphabet_index = alphabet.index(char)
-        encrypted_message += alphabet[(char_alphabet_index + shift) % 26]
+    @staticmethod
+    def decrypt(encrypted_message, shift):
+        """
+        """
+        pass
 
-    return encrypted_message
+    @staticmethod
+    def encrypt(plaintext_message, shift):
+        """
+            Encrypts a string using a Caesar Cipher with a user-provided shift.
+            Returns the encrypted message.
+        """
+        plaintext_message = plaintext_message.lower()
+        encrypted_message = ""
+
+        for char in plaintext_message:
+            
+            # Skip encryption if char is non-alphabetical
+            if char not in CaesarCipher.alphabet:
+                encrypted_message += char
+                continue
+
+            char_alphabet_index = CaesarCipher.alphabet.index(char)
+            encrypted_message += CaesarCipher.alphabet[(char_alphabet_index + shift) % 26]
+
+        return encrypted_message
 
 if __name__ == "__main__":
     while True:
         message = input("Enter a message you would like encrypted: ")
-        encrypted_message = caesar_encrypt(message, 17)
+        encrypted_message = CaesarCipher.encrypt(message, 1)
         print(f"Encrypted message: {encrypted_message}")
